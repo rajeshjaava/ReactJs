ce6f0f14c8c8741ac1ca9fd388222df008c3fa42
Rajesh <rajeshthokala10@gmail.com>
2020-08-08 15:07:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 const App=()=>{
     return (
 <div className ="ui container comments ">
+    <ApprovalCard>
+        <div><h1>
+        Are you sure you want this .?</h1>
+        </div>
+    </ApprovalCard>
+    <ApprovalCard>
     <CommentDetail author="Sam" timeAgo="Today at 10.45 AM" avatar={faker.image.avatar()} comment="Impressive blog"/>
-    <CommentDetail author="Alex" timeAgo ="Today at8.45 AM" avatar={faker.image.avatar()} comment="Nice blog"/> 
-    <CommentDetail author="Raj" timeAgo="Yesterday at 10pm"avatar={faker.image.avatar()} comment="Good blog"/> 
+    </ApprovalCard>
+    <ApprovalCard>
+       <CommentDetail author="Alex" timeAgo ="Today at8.45 AM" avatar={faker.image.avatar()} comment="Nice blog"/> 
+       </ApprovalCard>
+       <ApprovalCard>
+          <CommentDetail author="Raj" timeAgo="Yesterday at 10pm"avatar={faker.image.avatar()} comment="Good blog"/> 
+          </ApprovalCard>
     
 </div>
     );
